d06f7b555700eb219e82c5aec630b298436bb610
Mankirat Singh <mankirat1998@gmail.com>
2017-10-22 23:14:01 +0530

    feat: recipe-edit component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {DropDownDirective} from "./shared/dropdown.directive";
 import {ShoppingService} from "./shopping-list/shopping.service";
 import {AppRoutingModule} from "./app-routing.module";
 import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.componen
     ShoppingListComponent,
     ShoppingEditComponent,
     DropDownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
