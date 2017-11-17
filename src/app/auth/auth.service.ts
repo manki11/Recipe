338267d61537b0dde8eb01583e2b7d761ddc53d0
Mankirat Singh <mankirat1998@gmail.com>
2017-11-17 22:29:51 +0530

    feat: firebase signin added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,4 +8,14 @@ export class AuthService {
       );
   }
 
+  signInUser(email: string, password:string){
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(
+        response => console.log(response)
+      )
+      .catch(
+        error => console.log(error)
+      );
+  }
+
 }
