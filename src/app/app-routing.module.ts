60824d8df34c8f5c7ce9c0c5465a2178e6a796e6
Mankirat Singh <mankirat1998@gmail.com>
2017-11-23 23:27:49 +0530

    feat: home component and lazy loading added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {RecipesComponent} from "./recipes/recipes.component";
-import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
-import {SignupComponent} from "./auth/signup/signup.component";
-import {SigninComponent} from "./auth/signin/signin.component";
-import {AuthGaurdService} from "./auth/auth-gaurd.service";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {path: '', component: HomeComponent},
+  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,21 +10,21 @@ import {DataStorageService} from "./shared/data-storage.service";
 import {HttpModule} from "@angular/http";
 import {AuthService} from "./auth/auth.service";
 import {AuthGaurdService} from "./auth/auth-gaurd.service";
-import {RecipesModule} from "./recipes/recipes.module";
 import {SharedModule} from "./shared/shared.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {AuthModule} from "./auth/auth.module";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpModule,
-    RecipesModule,
     SharedModule,
     AuthModule,
     ShoppingListModule,
